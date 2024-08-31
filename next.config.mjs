5685a37adbc43eb5cf08561e9884abf20e26d2ec
Pedro Farias <phjfg@cesar.org.br>
2024-08-30 22:49:35 -0300

    feat: add base path to nextjs export

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "",
+  basePath: "/landing_page",
 
   /**
    * Disable server-based image optimization. Next.js does not support
