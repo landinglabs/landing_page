1fc37a3ec74cce55ba76acf7a829322dd51ff0ec
Luiz Machado <lfmm@cesar.org.br>
2024-09-02 18:46:57 -0300

    feat: editing features header

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,8 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24 overflow-hidden">
       <Hero />
       <Features header="Landing Page" description="Tenha uma landing page para alavancar seu negócio" bulletpoints={[{name:'Aumento das Conversões',description:'Aumento das Conversões'},{name:'Aumento das Conversões',description:'Aumento das Conversões'},{name:'Aumento das Conversões',description: 'Aumento das Conversões'}]} />
-      <Features header="Landing Page" description="Tenha uma landing page para alavancar seu negócio" bulletpoints={[{name:'Aumento das Conversões',description:'Aumento das Conversões'},{name:'Aumento das Conversões',description:'Aumento das Conversões'},{name:'Aumento das Conversões',description: 'Aumento das Conversões'}]}/>
-      <Features header="Landing Page" description="Tenha uma landing page para alavancar seu negócio" bulletpoints={[{name:'Aumento das Conversões',description:'Aumento das Conversões'},{name:'Aumento das Conversões',description:'Aumento das Conversões'},{name:'Aumento das Conversões',description: 'Aumento das Conversões'}]}/>
+      <Features header="E-Commerce" description="Tenha uma landing page para alavancar seu negócio" bulletpoints={[{name:'Aumento das Conversões',description:'Aumento das Conversões'},{name:'Aumento das Conversões',description:'Aumento das Conversões'},{name:'Aumento das Conversões',description: 'Aumento das Conversões'}]}/>
+      <Features header="CRM" description="Tenha uma landing page para alavancar seu negócio" bulletpoints={[{name:'Aumento das Conversões',description:'Aumento das Conversões'},{name:'Aumento das Conversões',description:'Aumento das Conversões'},{name:'Aumento das Conversões',description: 'Aumento das Conversões'}]}/>
       <Stats />
       <Contacts />
       <FAQ />
