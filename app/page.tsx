7faff8b2ef1367cf653d2d151ab8a644cdf4450c
Pedro Farias <phjfg@cesar.org.br>
2024-09-08 19:15:25 -0300

    feat: add background to hero section

--- app/page.tsx
+++ app/page.tsx
@@ -6,11 +6,9 @@ import { Stats } from "@/components/sections/stats";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24 overflow-hidden">
+    <main>
       <Hero />
-      <Features header="Soluções Digitais" description="Tenha um site de alta conversão e/ou aplicativo e faça seu negócio decolar!" bulletpoints={[{ name: 'Aplicativos Sob Medida', description: 'Leve sua ideia para o mobile com um app envolvente e funcional!' }, { name: 'Websites Poderosos', description: 'Tenha um site rápido, seguro e pronto para converter mais!' }, { name: 'E-commerce de Alta Conversão', description: 'Venda mais com uma loja virtual personalizada e eficiente!' }]} anchor={"digitalsolution"} />
-      <Features header="Conexões Inteligentes" description="Automatize processos e conecte suas plataformas para operar sem limites!" bulletpoints={[{ name: 'APIs Personalizadas', description: 'Conecte sistemas de forma rápida e segura com APIs feitas sob medida!' }, { name: 'Integração de Sistemas', description: 'Unifique plataformas e automações para otimizar sua operação!' }, { name: 'Automação Inteligente', description: 'Aumente a eficiência automatizando processos e eliminando retrabalhos!' }]} anchor={"smartintegration"}/>
-      <Features header="IA Aplicada" description="Acelere seu crescimento com soluções de inteligência artificial sob medida!" bulletpoints={[{ name: 'IA para Negócios', description: 'Potencialize seu crescimento com soluções de IA sob medida!' }, { name: 'Automação Inteligente', description: 'Aumente a produtividade automatizando tarefas complexas!' }, { name: 'Análises Preditivas', description: 'Antecipe tendências e tome decisões mais assertivas com IA!' }]} anchor={"ai"}/>
+      <Features />
       <Stats />
       <FAQ />
     </main>
--- components/sections/hero.tsx
+++ components/sections/hero.tsx
@@ -2,27 +2,34 @@ import { MoveRight, PhoneCallIcon, RocketIcon } from "lucide-react";
 import { Button } from "../ui/button";
 
 export const Hero = () => (
-  <div className="w-full">
-    <div className="class-element-home-anchor" id='home'></div>
-    <div className="container mx-auto">
+  <div className="w-full relative overflow-hidden py-12 lg:py-16 ">
+    <div
+      aria-hidden="true"
+      className="flex absolute -top-96 start-1/2 transform -translate-x-1/2 -z-10"
+    >
+      <div className=" -z-10 bg-gradient-to-r from-background/50 to-background blur-3xl w-[25rem] h-[44rem] rotate-[-60deg] transform -translate-x-[10rem]" />
+      <div className=" -z-10 bg-gradient-to-tl blur-3xl w-[90rem] h-[50rem] rounded-full origin-top-left -rotate-12 -translate-x-[15rem] from-primary-foreground via-primary-foreground to-background" />
+    </div>
+    <div className="class-element-home-anchor" id="home"></div>
+    <div className="container mx-auto z-10 px-8">
       <div className="flex gap-8 py-20 lg:py-40 items-center justify-center flex-col">
-
         <div className="flex gap-4 flex-col">
-          <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular">
-          Desbloqueando Inovações, Moldando o Futuro
+          <h1 className="text-5xl md:text-7xl max-w-2xl tracking-tighter text-center font-regular font-bold">
+            Desbloqueando Inovações, Moldando o Futuro
           </h1>
           <p className="text-lg md:text-xl leading-relaxed tracking-tight text-muted-foreground max-w-2xl text-center">
-            Aumente o potencial da tecnologia com nossas soluções de ponta, projetadas para impulsionar o seu sucesso.
+            Aumente o potencial da tecnologia com nossas soluções de ponta,
+            projetadas para impulsionar o seu sucesso.
           </p>
         </div>
-          <div className="flex flex-col md:flex-row gap-4">
-            <Button size="lg" className="gap-4" variant="outline">
+        <div className="flex flex-col md:flex-row gap-4">
+          <Button size="lg" className="gap-4" variant="outline">
             Venha explorar mais <RocketIcon className="w-4 h-4" />
-            </Button>
-            <Button size="lg" className="gap-4">
+          </Button>
+          <Button size="lg" className="gap-4">
             Comece agora! <PhoneCallIcon className="w-4 h-4" />
-            </Button>
-          </div>
+          </Button>
+        </div>
       </div>
     </div>
   </div>
