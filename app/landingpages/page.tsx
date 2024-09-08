e1378f5fb7f495e01512a2c1bb2236be971fe5c0
Pedro Farias <phjfg@cesar.org.br>
2024-09-08 19:29:11 -0300

    fix: remove feature from page

--- app/landingpages/page.tsx
+++ app/landingpages/page.tsx
@@ -4,7 +4,6 @@ import { Pricing } from "@/components/sections/pricing";
 export default function LandingPages() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-8 md:p-24 overflow-hidden">
-      <Features header="Landing Page" description="Tenha uma landing page para alavancar seu negócio" bulletpoints={[{ name: 'Design Personalizado', description: 'Alinhado à sua marca para maior engajamento.' }, { name: 'Otimização para Conversões', description: 'Transforme visitantes em leads qualificados.' }, { name: 'Integração de Marketing', description: 'Conecte-se facilmente às suas ferramentas de marketing.' }]} anchor={"landingpage"} />
       <Pricing></Pricing>
     </main>
   );
