24f893fcb59c31ef1096b0d9d30f8803d1676103
Luiz Machado <luizmaachado@gmail.com>
2025-12-01 18:32:40 -0300

    feat: add privacy policy to reverse audio app

---
+++ app/privacy_policy/reverse_audio/page.tsx
@@ -0,0 +1,121 @@
+export default function ReverseAudioPrivacyPolicy() {
+    return (
+        <div
+            dangerouslySetInnerHTML={{
+                __html: `
+          <!DOCTYPE html>
+          <html>
+            <head>
+              <meta charset='utf-8'>
+              <meta name='viewport' content='width=device-width'>
+              <title>Política de Privacidade</title>
+              <style>
+                body {
+                  font-family: 'Helvetica Neue', Helvetica, Arial, sans-serif;
+                  padding: 1em;
+                }
+              </style>
+            </head>
+            <body>
+              <strong>Política de Privacidade</strong>
+              <p>
+                Esta política de privacidade se aplica ao aplicativo Audio Invertido (doravante referido como "Aplicativo")
+                para dispositivos móveis que foi criado pela LM Dev INC (doravante referida como "Provedor de Serviços")
+                como um serviço com suporte de anúncios. Este serviço é destinado ao uso "COMO ESTÁ".
+              </p>
+              <br>
+              
+              <strong>Quais informações o Aplicativo obtém e como são usadas?</strong>
+              <p>
+                O Aplicativo não obtém nenhuma informação quando você o baixa e usa.
+                Registro não é necessário para usar o Aplicativo.
+              </p>
+              <br>
+              
+              <strong>O Aplicativo coleta informações precisas de localização em tempo real do dispositivo?</strong>
+              <p>
+                Este Aplicativo não coleta informações precisas sobre a localização do seu dispositivo móvel.
+              </p>
+              <br>
+              
+              <strong>Terceiros veem e/ou têm acesso às informações obtidas pelo Aplicativo?</strong>
+              <p>
+                Como o Aplicativo não coleta nenhuma informação, nenhum dado é compartilhado com terceiros.
+              </p>
+              <br>
+              
+              <strong>Quais são meus direitos de exclusão?</strong>
+              <p>
+                Você pode parar toda a coleta de informações pelo Aplicativo facilmente desinstalando-o.
+                Você pode usar os processos de desinstalação padrão que podem estar disponíveis como parte do seu dispositivo móvel
+                ou através do mercado de aplicativos móveis ou rede.
+              </p>
+              <br>
+              
+              <strong>Crianças</strong>
+              <p>
+                O Aplicativo não é usado para solicitar intencionalmente dados de ou comercializar para crianças menores de 13 anos.
+              </p>
+              <br>
+              <p>
+                O Provedor de Serviços não coleta intencionalmente informações de identificação pessoal de crianças.
+                O Provedor de Serviços incentiva todas as crianças a nunca enviar qualquer informação de identificação pessoal
+                através do Aplicativo e/ou Serviços. O Provedor de Serviços incentiva pais e responsáveis legais
+                a monitorar o uso da Internet de seus filhos e ajudar a aplicar esta Política, instruindo seus
+                filhos a nunca fornecer informações de identificação pessoal através do Aplicativo e/ou Serviços
+                sem sua permissão. Se você tiver motivos para acreditar que uma criança forneceu informações de identificação pessoal
+                ao Provedor de Serviços através do Aplicativo e/ou Serviços, entre em
+                contato com o Provedor de Serviços (luizmaachado@gmail.com) para que eles possam tomar as medidas
+                necessárias. Você também deve ter pelo menos 16 anos de idade para consentir com o processamento de suas
+                informações de identificação pessoal em seu país (em alguns países podemos permitir que seu pai ou responsável faça
+                isso em seu nome).
+              </p>
+              <br>
+              
+              <strong>Segurança</strong>
+              <p>
+                O Provedor de Serviços está preocupado em proteger a confidencialidade de suas informações. No entanto,
+                como o Aplicativo não coleta nenhuma informação, não há risco de seus dados serem acessados
+                por indivíduos não autorizados.
+              </p>
+              <br>
+              
+              <strong>Alterações</strong>
+              <p>
+                Esta Política de Privacidade pode ser atualizada de tempos em tempos por qualquer motivo. O Provedor de Serviços notificará
+                você sobre quaisquer alterações em sua Política de Privacidade, atualizando esta página com a nova Política de Privacidade. Você é
+                aconselhado a consultar esta Política de Privacidade regularmente para quaisquer alterações, pois o uso continuado é considerado aprovação
+                de todas as alterações.
+              </p>
+              <br>
+              <p>
+                Esta política de privacidade é efetiva a partir de 01-12-2025
+              </p>
+              <br>
+              
+              <strong>Seu Consentimento</strong>
+              <p>
+                Ao usar o Aplicativo, você está consentindo com o processamento de suas informações conforme estabelecido nesta
+                Política de Privacidade agora e conforme alterado pelo Provedor de Serviços.
+              </p>
+              <br>
+              
+              <strong>Entre em Contato Conosco</strong>
+              <p>
+                Se você tiver alguma dúvida sobre privacidade ao usar o Aplicativo, ou tiver dúvidas sobre as
+                práticas, entre em contato com o Provedor de Serviços por e-mail em luizmaachado@gmail.com.
+              </p>
+              <hr>
+              <p>
+                Esta página de política de privacidade foi gerada por
+                <a href="https://app-privacy-policy-generator.nisrulz.com/" target="_blank" rel="noopener noreferrer">
+                  App Privacy Policy Generator
+                </a>
+              </p>
+            </body>
+          </html>
+        `,
+            }}
+        />
+    );
+}
